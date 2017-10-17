e9641b85a668adb142b3d455f6aad88770c224cc
Nick Main <nickmaster24@gmail.com>
2017-10-16 20:43:15 -0400

    Added entities for all items in the database

--- SoftwareEngineeringPizzaOrderApplication/Form1.Designer.cs
+++ SoftwareEngineeringPizzaOrderApplication/Form1.Designer.cs
@@ -29,11 +29,57 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.pizzaApplicationDatabaseDataSet = new SoftwareEngineeringPizzaOrderApplication.PizzaApplicationDatabaseDataSet();
+            this.customerBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.customerTableAdapter = new SoftwareEngineeringPizzaOrderApplication.PizzaApplicationDatabaseDataSetTableAdapters.CustomerTableAdapter();
+            this.tableAdapterManager = new SoftwareEngineeringPizzaOrderApplication.PizzaApplicationDatabaseDataSetTableAdapters.TableAdapterManager();
+            ((System.ComponentModel.ISupportInitialize)(this.pizzaApplicationDatabaseDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.customerBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pizzaApplicationDatabaseDataSet
+            // 
+            this.pizzaApplicationDatabaseDataSet.DataSetName = "PizzaApplicationDatabaseDataSet";
+            this.pizzaApplicationDatabaseDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // customerBindingSource
+            // 
+            this.customerBindingSource.DataMember = "Customer";
+            this.customerBindingSource.DataSource = this.pizzaApplicationDatabaseDataSet;
+            // 
+            // customerTableAdapter
+            // 
+            this.customerTableAdapter.ClearBeforeFill = true;
+            // 
+            // tableAdapterManager
+            // 
+            this.tableAdapterManager.AddressTableAdapter = null;
+            this.tableAdapterManager.BackupDataSetBeforeUpdate = false;
+            this.tableAdapterManager.CustomerTableAdapter = this.customerTableAdapter;
+            this.tableAdapterManager.OrderItemTableAdapter = null;
+            this.tableAdapterManager.OrderTableAdapter = null;
+            this.tableAdapterManager.UpdateOrder = SoftwareEngineeringPizzaOrderApplication.PizzaApplicationDatabaseDataSetTableAdapters.TableAdapterManager.UpdateOrderOption.InsertUpdateDelete;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(823, 490);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pizzaApplicationDatabaseDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.customerBindingSource)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private PizzaApplicationDatabaseDataSet pizzaApplicationDatabaseDataSet;
+        private System.Windows.Forms.BindingSource customerBindingSource;
+        private PizzaApplicationDatabaseDataSetTableAdapters.CustomerTableAdapter customerTableAdapter;
+        private PizzaApplicationDatabaseDataSetTableAdapters.TableAdapterManager tableAdapterManager;
     }
 }
 
--- SoftwareEngineeringPizzaOrderApplication/Form1.cs
+++ SoftwareEngineeringPizzaOrderApplication/Form1.cs
@@ -16,5 +16,20 @@ namespace SoftwareEngineeringPizzaOrderApplication
         {
             InitializeComponent();
         }
+
+        private void customerBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.customerBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.pizzaApplicationDatabaseDataSet);
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'pizzaApplicationDatabaseDataSet.Customer' table. You can move, or remove it, as needed.
+            this.customerTableAdapter.Fill(this.pizzaApplicationDatabaseDataSet.Customer);
+
+        }
     }
 }
